dac48ed92d446848bafe894e5f0a56644969934b
Vivienfanghua <vivienfanghua@gmail.com>
2018-04-26 14:53:00 +0800

    api

--- index.js
+++ index.js
@@ -3,20 +3,85 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
+
 let mongodb;
+let data,timeIndexData,allConflictCodes,allBarriers;
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     mongodb = db.db("localhost");
+    timeIndexData = {};
+    allBarriers = {};
+    allConflictCodes = [];
+    mongodb.collection("test2"). find({}).toArray(function(err, result) {
+        if (err) throw err;
 
+        data = result;
+        data.forEach((item, i) => {
+            // console.log(item);
+            if(item['data_exist']){
+                let time_data_items = item['data'];
+                let conflict = false;
+                time_data_items.forEach((time_item, t_i) =>{
+                    let time = time_item['time'];
+                    let barrierId = time_item['dataid'];
+                    if(time_item['conflict'])
+                        conflict = true;
+                    if(!timeIndexData[time]){
+                        timeIndexData[time] = [];
+                    }
+                    timeIndexData[time].push(time_item);
+                    if(!allBarriers[barrierId]){
+                        allBarriers[barrierId] = {};
+                    }
+                    allBarriers[barrierId][time] = time_item;
+                });
+                if(conflict){
+                    allConflictCodes.push(item['_id']);
+                }
+            }
+        });
+    });
 });
 
+/**
+ * @description test.
+ */
 app.get('/', function (req, res) {
-    mongodb.collection("test"). find({}).toArray(function(err, result) { // 返回集合中所有数据
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
-    });
+    res.send('holy g3is!');
+});
+
+/**
+ * @example http://127.0.0.1:3000/get_all_conflict_codes
+ * @return JSON array.
+ */
+app.get('/get_all_conflict_codes',function (req, res) {
+    console.log('get_all_conflict_codes');
+    res.send(allConflictCodes);
+});
+
+/**
+ * @param req.query.time int
+ * @example http://127.0.0.1:3000/get_current_data?time=55
+ * @return JSON array.
+ */
+app.get('/get_current_data',function (req, res) {
+    let queryTime = req.query.time;
+    console.log('get_current_data',queryTime);
+    res.send(timeIndexData[queryTime]);
+});
+
+/**
+ * @param req.query.dataid int
+ * @param req.query.time int
+ * @example http://127.0.0.1:3000/get_barrier_info?dataid=8&time=6197
+ * @return JSON object.
+ */
+app.get('/get_barrier_info',function (req, res) {
+    let dataId = req.query.dataid;
+    let queryTime = req.query.time;
+    console.log('get_barrier_info',dataId, queryTime);
+    res.send(allBarriers[dataId][queryTime]);
 });
 
 app.listen(3000, function () {
